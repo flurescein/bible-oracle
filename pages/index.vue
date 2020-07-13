ab96d15fe0c56f9312548a2790455ac2833944a8
Vlad Kamyshov <flurescein@gmail.com>
2020-07-13 12:13:11 +0300

    Remove unused import.

--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,12 @@
 <script>
 import { mapActions } from 'vuex'
 
-import Localized from '~/components/Localized.vue'
 import Verse from '~/components/Verse.vue'
 import LanguageSwitcher from '~/components/LanguageSwitcher.vue'
 import RandomizeButton from '~/components/RandomizeButton.vue'
 
 export default {
-  components: { Localized, Verse, LanguageSwitcher, RandomizeButton },
+  components: { Verse, LanguageSwitcher, RandomizeButton },
   methods: { ...mapActions(['randomizeVerse']) },
   mounted() {
     this.randomizeVerse()
